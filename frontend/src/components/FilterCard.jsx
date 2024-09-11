1fefe90913701add68ee7b586442f9fa034bc49b
sachin kunwar <sachinkunwar284@gmail.com>
2024-09-11 13:44:33 +0545

    first commit

---
+++ frontend/src/components/FilterCard.jsx
@@ -0,0 +1,61 @@
+
+import React, { useEffect, useState } from 'react'
+import { RadioGroup, RadioGroupItem } from './ui/radio-group'
+import { Label } from './ui/label'
+import { useDispatch } from 'react-redux'
+import { setSearchedQuery } from '@/redux/jobSlice'
+
+
+const fitlerData = [
+    {
+        fitlerType: "Location",
+        array: ["Kathmandu", "Pokhara", "Lalitpur", "Biratnagar","Butwal"]
+    },
+    {
+        fitlerType: "Industry",
+        array: ["Frontend Developer", "Backend Developer", "FullStack Developer","UI/UX Designer","Data Scientist"  ]
+    },
+    {
+        fitlerType: "Salary",
+        array: ["0-30k", "30k-60k", "60k-100k","100k-150k","More than 150k"]
+    },
+]
+
+const FilterCard = () => {
+    const [selectedValue, setSelectedValue] = useState('');
+    const dispatch = useDispatch();
+    const changeHandler = (value) => {
+        setSelectedValue(value);
+    }
+    useEffect(()=>{
+        dispatch(setSearchedQuery(selectedValue));
+    },[selectedValue]);
+    return (
+        <div className='w-full bg-white p-3 rounded-md'>
+            <h1 className='font-bold text-lg'>Filter Jobs</h1>
+            <hr className='mt-3' />
+            <RadioGroup value={selectedValue} onValueChange={changeHandler}>
+                {
+                    fitlerData.map((data, index) => (
+                        <div>
+                            <h1 className='font-bold text-lg'>{data.fitlerType}</h1>
+                            {
+                                data.array.map((item, idx) => {
+                                    const itemId = `id${index}-${idx}`
+                                    return (
+                                        <div className='flex items-center space-x-2 my-2'>
+                                            <RadioGroupItem value={item} id={itemId} />
+                                            <Label htmlFor={itemId}>{item}</Label>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    ))
+                }
+            </RadioGroup>
+        </div>
+    )
+}
+
+export default FilterCard
